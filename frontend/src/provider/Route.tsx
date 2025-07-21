3d86318b731981bb3e466af065b927b83273fd9e
payal-04 <payal078601payal@gmail.com>
2025-07-21 17:41:16 +0530

    commited

--- frontend/src/provider/Route.tsx
+++ frontend/src/provider/Route.tsx
@@ -1,3 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
-export const Routes = createBrowserRouter()
+export const Routes = createBrowserRouter([
+    {
+        path: '/',
+        Component: App
+    },
+    {
+        path: '/login',
+        Component: Login
+    },
+    {
+        path: '/register',
+        Component: Register
+    }
+])
